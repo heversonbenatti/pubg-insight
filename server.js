06eca53da8760c0a2a6ff3b73a7009323b5eb504
Heverson Benatti <heversonbenatti@hotmail.com>
2024-10-02 11:23:51 -0300

    Cleaned up comments and logs

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
 
-const API_KEY = process.env.API_KEY;  // Carregar o token da variável de ambiente
+const API_KEY = process.env.API_KEY;
 const BASE_URL = 'https://api.pubg.com/shards/steam';
 
 app.use(express.static('public'));
 
-// Endpoint para listar temporadas
 app.get('/api/seasons', async (req, res) => {
     try {
         const response = await axios.get(`${BASE_URL}/seasons`, {
@@ -17,14 +15,12 @@ app.get('/api/seasons', async (req, res) => {
                 'Accept': 'application/vnd.api+json'
             }
         });
-        res.json(response.data.data);  // Envia os dados das temporadas para o frontend
+        res.json(response.data.data);
     } catch (error) {
-        console.error(error.response ? error.response.data : error.message);
         res.json({ error: 'Could not fetch seasons' });
     }
 });
 
-// Endpoint para buscar estatísticas do jogador
 app.get('/api/player/:playerName', async (req, res) => {
     const { playerName } = req.params;
     const { season, isFPP } = req.query;
@@ -39,7 +35,6 @@ app.get('/api/player/:playerName', async (req, res) => {
         });
 
         const playerId = playerResponse.data.data[0].id;
-
         const seasonStatsResponse = await axios.get(`${BASE_URL}/players/${playerId}/seasons/${season}`, {
             headers: {
                 'Authorization': `Bearer ${API_KEY}`,
@@ -56,12 +51,10 @@ app.get('/api/player/:playerName', async (req, res) => {
 
         res.json({ player: { name: playerName }, stats: playerStats });
     } catch (error) {
-        console.error(error.response ? error.response.data : error.message);
         res.json({ error: 'Player not found or API error' });
     }
 });
 
-// Iniciar servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
