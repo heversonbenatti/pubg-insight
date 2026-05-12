9c91579a93a3e97b56e456cfc3856006bacec0fb
Heverson Benatti <heversonbenatti@hotmail.com>
2026-05-12 14:37:21 -0300

    Change server to listen on all interfaces

--- server.js
+++ server.js
@@ -423,4 +423,6 @@ app.get('/api/telemetry/:matchId', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
